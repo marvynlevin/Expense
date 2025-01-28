0c008f52672ae38c5e89f418413b7c19e956b3a9
marvynlevin <marvyn.levin25460@gmail.com>
2025-01-28 09:24:39 +0100

    Initial Commit

---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by levin marvyn on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
